263eb3687d317e245e32889c17a46eb1d48f16ff
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2023-09-21 15:52:54 +0200

    init commit

---
+++ forge.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+  name: '@electron-forge/maker-flatpak',
+  config: {
+    options: {
+      "categories": ['Utility']
+    }
+  }
+}
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
